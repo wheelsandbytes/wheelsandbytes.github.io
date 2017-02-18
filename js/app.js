bb1f76de725aeb0736752c286812640c1cb234f9
Tim <wheelsandbytes@gmail.com>
2017-02-17 21:57:15 -0500

    redesigned and restructured

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function AppController($scope, $http) {
+
+    $scope.selectedArticle = "";
+
+    $http.get('../_articles.json').success(function(data) {
+        $scope.articles = data;
+    });
+
+    $scope.getArticleTitle = function(item) {
+        $scope.selectedArticle = item;
+        return item.title;
+    };
+
+    $scope.getArticleLink = function(item) {
+        $scope.selectedArticle = item;
+        return item.page;
+    };
+
+    $scope.getArticlePath = function(item) {
+        $scope.selectedArticle = item;
+        return item.path;
+    };
+
+    $scope.getArticleThumbnail = function(item) {
+        $scope.selectedArticle = item;
+        return item.thumbnail;
+    };
+
+    $scope.getArticleType = function(item) {
+        $scope.selectedArticle = item;
+        return item.type;
+    };
+
+    $scope.getArticleDescription = function(item) {
+        $scope.selectedArticle = item;
+        return item.description;
+    };
+}
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+   $('.materialboxed').materialbox();
+   $(".dropdown-button").dropdown();
+    $('.parallax').parallax();
+ });
