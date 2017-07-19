188e675c820cbe723f2fe18ac769327a1e9ed01f
Tim <tim.alex.ingle@gmail.com>
2017-07-19 01:46:14 -0400

    updated comp page

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $(".button-collapse").sideNav();
     $('.slider').slider();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 });
